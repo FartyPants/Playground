3edbbe499b945356a8b2ed160dfe07ec1e19eb3c
FartyPants <fartypantsham@gmail.com>
2023-06-09 02:58:06 -0400

    Added generate from selection

--- script.py
+++ script.py
@@ -1,179 +1,161 @@
+from pathlib import Path
 import gradio as gr
-import pickle
-import modules.shared as shared
-from modules.extensions import apply_extensions
-from modules.text_generation import encode, get_max_prompt_length
-from modules.text_generation import generate_reply
-from modules.text_generation import generate_reply_wrapper
-from modules.text_generation import stop_everything_event
-from modules.ui import list_interface_input_elements
-from modules.ui import gather_interface_values
-from modules.html_generator import generate_basic_html
+from modules import utils
+from modules import shared
+from modules.models import unload_model
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from peft import PeftModel
+from pathlib import Path
+import torch
+import math
+import os
 
-try:
-    with open('notebook.sav', 'rb') as f:
-        params = pickle.load(f)
-except FileNotFoundError:
-    params = {
-        "display_name": "Playground",
-        "is_tab": True,
-        "usePR": False,
-        "pUSER": 'USER:',
-        "pBOT": 'ASSISTANT:'
-    }
+params = {
+    "display_name": "Merge",
+    "is_tab": True,
+}
 
-def get_last_line(string):
-    lines = string.splitlines()
-    if lines:
-        last_line = lines[-1]
-        return last_line
-    else:
-        return ""
+refresh_symbol = '\U0001f504'  # 🔄
+
+class ToolButton(gr.Button, gr.components.FormComponent):
+    """Small button with single emoji as text, fits inside gradio forms"""
+
+    def __init__(self, **kwargs):
+        super().__init__(variant="tool", **kwargs)
+
+    def get_block_name(self):
+        return "button"
 
 
-def input_modifier(string):
+def create_refresh_button(refresh_component, refresh_method, refreshed_args, elem_id):
+    def refresh():
+        refresh_method()
+        args = refreshed_args() if callable(refreshed_args) else refreshed_args
 
-    modified_string = string
-    addLineReply = ""
+        for k, v in args.items():
+            setattr(refresh_component, k, v)
 
-    if params['usePR']:
-        if "---" in string:
-            lines = string.splitlines()  # Split the text into lines
-            modified_string = ""
-            for i, line in enumerate(lines):
-                if addLineReply:
-                    line.lstrip()
-                    line = addLineReply + line
-                    addLineReply = ""
-                elif line.startswith("---"):
-                    line = line.replace("---", params['pUSER'])  
-                    addLineReply = params['pBOT'] 
-                    
-                modified_string = modified_string+ line +"\n"
+        return gr.update(**(args or {}))
 
-            if addLineReply:
-                modified_string = modified_string + addLineReply
+    refresh_button = ToolButton(value=refresh_symbol, elem_id=elem_id)
+    refresh_button.click(
+        fn=refresh,
+        inputs=[],
+        outputs=[refresh_component]
+    )
+    return refresh_button
 
-    return modified_string
-
-
+def process_merge(model_name, peft_model_name, output_dir):
     
-def output_modifier(string):
-    #print(f"output_modifier: {string}") 
-    return string
+    base_model_name_or_path = Path(f'{shared.args.model_dir}/{model_name}')
+    peft_model_path = Path(f'{shared.args.lora_dir}/{peft_model_name}')
+    device_arg = { 'device_map': 'auto' }
+    print(f"Unloading model")
+    unload_model()
 
-def copynote(string):
-    return string
+    print(f"Loading base model: {base_model_name_or_path}")
+  
+    try:
+        base_model = AutoModelForCausalLM.from_pretrained(
+            base_model_name_or_path,
+            return_dict=True,
+            torch_dtype=torch.float16,
+            device_map={'': 0})    
+    except Exception as e:
+        print(f"\033[91mError in AutoModelForCausalLM: \033[0;37;0m\n {e}" )
+        print(f"Merge failed")
+        return
+    
+    print(f"Loading PEFT: {peft_model_path}")
+    try:
+        model = PeftModel.from_pretrained(base_model, peft_model_path, torch_dtype=torch.float16, device_map={'': 0})
+    except Exception as e:
+        print(f"\033[91mError initializing PeftModel:  \033[0;37;0m\n{e}")
+        print(f"Merge failed")
+        return
 
-def formatted_outputs(reply):
- 
-    return reply, generate_basic_html(reply)
+    print(f"Running merge_and_unload - WAIT untill you see the Model saved message")
+    model = model.merge_and_unload()
+    try:
+        tokenizer = AutoTokenizer.from_pretrained(base_model_name_or_path)
+    except Exception as e:
+        print(f"\033[91mError in AutoTokenizer:  \033[0;37;0m\n{e}")
+        print(f"Merge failed")
+        return
+    model.save_pretrained(f"{output_dir}")
+    tokenizer.save_pretrained(f"{output_dir}")
+    print(f"Model saved to {output_dir}")
+    print(f"**** DONE ****")
+
+def clean_path(base_path: str, path: str):
+    """Strips unusual symbols and forcibly builds a path as relative to the intended directory."""
+    # TODO: Probably could do with a security audit to guarantee there's no ways this can be bypassed to target an unwanted path.
+    # Or swap it to a strict whitelist of [a-zA-Z_0-9]
+    path = path.replace('\\', '/').replace('..', '_')
+    if base_path is None:
+        return path
+
+    return f'{Path(base_path).absolute()}/{path}'
 
 
-def generate_reply_wrapperMY(question, state):
 
-    # if use quick prompt, add \n if none
-    if params['usePR']:
-        if not question.endswith("\n"):
-            lastline = get_last_line(question)
-            if lastline.startswith("---"):
-                question+="\n"
-
-    for reply in generate_reply(question, state, eos_token = None, stopping_strings=None, is_chat=False):
-        if shared.model_type not in ['HF_seq2seq']:
-            reply = question + reply
-
-        yield formatted_outputs(reply)
+def estimate_proc(raw_text_file, micro_batch_size,batch_size,steps):
+    
+    epochs = 1
+    if raw_text_file not in ['None', '']:
+        file_path = clean_path('training/datasets', f'{raw_text_file}.txt')
+        file_size = os.path.getsize(file_path)
+        #print(f"bytecount {file_size}")
+     
+        # this is really just made to fit the situation, it's bad math, but I'm too tired
+        # one day....
+        cutoff_len = 256
+        overlap_len = 128
+        #epochs = math.ceil((steps * micro_batch_size * (cutoff_len - overlap_len)) / (0.3333333 * file_size))
+        epochs =  math.ceil((steps * (128/micro_batch_size) * (cutoff_len - overlap_len)) / ((2.62144 * file_size)))
+    
+    return str(epochs)
 
 def ui():
-    #input_elements = list_interface_input_elements(chat=False)
-    #interface_state = gr.State({k: None for k in input_elements})
-
-    with gr.Row():
-        with gr.Column():
-            with gr.Row():
-                with gr.Tab('Text'):
-                    with gr.Row():
-                        text_boxA = gr.Textbox(value='', elem_classes="textbox", lines=20, label = 'Notebook A')
-                with gr.Tab('HTML'):
-                    with gr.Row():
-                        htmlA = gr.HTML()
-            with gr.Row():    
-                generate_btnA = gr.Button('Generate', variant='primary', elem_classes="small-button")
-                stop_btnA = gr.Button('Stop', elem_classes="small-button")
-                toNoteB = gr.Button('Copy to B', elem_classes="small-button")
-            with gr.Row():
-                with gr.Box():
-                    with gr.Column():    
-                        usePR = gr.Checkbox(value=params['usePR'], label='Enable Quick Instruct (line starts with three dashes --- )')
-                        with gr.Row():
-                            preset_type = gr.Dropdown(label="Preset", choices=["Custom", "Vicuna", "Alpaca", "Guanaco"], value="Custom")
-                            text_USR = gr.Textbox(value=params['pUSER'], lines=1, label='User string')
-                            text_BOT = gr.Textbox(value=params['pBOT'], lines=1, label='Bot string')
-                        gr.Markdown('Example: --- What are synonyms for happy?')    
-
-        with gr.Column():
-            with gr.Row():
-                with gr.Tab('Text'):
-                    with gr.Row():
-                        text_boxB = gr.Textbox(value='', elem_classes="textbox", lines=20, label = 'Notebook B')
-                with gr.Tab('HTML'):
-                    with gr.Row():
-                        htmlB = gr.HTML()
-            with gr.Row():    
-                generate_btnB = gr.Button('Generate', variant='primary', elem_classes="small-button")
-                stop_btnB = gr.Button('Stop', elem_classes="small-button")
-                toNoteA = gr.Button('Copy to A', elem_classes="small-button")
-            with gr.Row():    
-                gr.Markdown('    (v.0.9    FPham 2023)')
-
-   
-        #shared.gradio['Undo'] = gr.Button('Undo', elem_classes="small-button")
-        #shared.gradio['Regenerate'] = gr.Button('Regenerate', elem_classes="small-button")
-    # Todo:
-    # add silider for independend temperature, top_p and top_k
-    # shared.input_elements, shared.gradio['top_p'] 
-    input_paramsA = [text_boxA,shared.gradio['interface_state']]
-    output_paramsA =[text_boxA, htmlA]
-    input_paramsB = [text_boxB,shared.gradio['interface_state']]
-    output_paramsB =[text_boxB, htmlB]
-   
-    generate_btnA.click(gather_interface_values, [shared.gradio[k] for k in shared.input_elements], shared.gradio['interface_state']).then(
-        generate_reply_wrapperMY, inputs=input_paramsA, outputs=output_paramsA, show_progress=False)
-    stop_btnA.click(stop_everything_event, None, None, queue=False)
-    generate_btnB.click(gather_interface_values, [shared.gradio[k] for k in shared.input_elements], shared.gradio['interface_state']).then(
-        generate_reply_wrapperMY, inputs=input_paramsB, outputs=output_paramsB, show_progress=False)
-    stop_btnB.click(stop_everything_event, None, None, queue=False)
-    toNoteB.click(copynote, text_boxA, text_boxB)    
-    toNoteA.click(copynote, text_boxB, text_boxA)
-    
-    def update_activate(x):
-        params.update({"usePR": x})
-        with open('notebook.sav', 'wb') as f:
-            pickle.dump(params, f)
-    
-    def update_stringU(x):
-        params.update({"pUSER": x})
-        with open('notebook.sav', 'wb') as f:
-            pickle.dump(params, f)
-    def update_stringB(x):
-        params.update({"pBOT": x})
-        with open('notebook.sav', 'wb') as f:
-            pickle.dump(params, f)
-    
-    def update_preset(x):
-        if x == "Vicuna":
-            return 'USER:','ASSISTANT:'
-        elif x == "Alpaca":
-            return '### Instruction:','### Response:'
-        elif x == "Guanaco":
-            return '### Human:','### Assistant:'
+    model_name = "None"
+    lora_names = "None"
+    with gr.Accordion("Merge Model with Lora", open=True):
         
-        return 'USER:','ASSISTANT:'           
+        with gr.Row():
+               
+            with gr.Column():
+                with gr.Row():
+                    gr_modelmenu = gr.Dropdown(choices=utils.get_available_models(), value=model_name, label='Model 16-bit HF only')
+                    create_refresh_button(gr_modelmenu, lambda: None, lambda: {'choices': utils.get_available_models()}, 'refresh-button')
 
+            with gr.Column():
+                with gr.Row():
+                    gr_loramenu = gr.Dropdown(multiselect=False, choices=utils.get_available_loras(), value=lora_names, label='LoRA')
+                    create_refresh_button(gr_loramenu, lambda: None, lambda: {'choices': utils.get_available_loras(), 'value': lora_names}, 'refresh-button')
 
-    usePR.change(update_activate, usePR, None)   
-    text_USR.change(update_stringU, text_USR, None) 
-    text_BOT.change(update_stringB, text_BOT, None) 
-    preset_type.change(update_preset,preset_type,[text_USR,text_BOT])
+        output_dir = gr.Textbox(label='Output Dir', info='The folder name of your merge (relative to text-generation-webui)')
+        gr_apply = gr.Button(value='Do Merge')    
+        gr_apply.click(process_merge, inputs=[gr_modelmenu, gr_loramenu,output_dir])
+
+    with gr.Accordion("LORA Training Epoch Estimator for plaintext (half-assed)", open=False):
+        with gr.Row():
+            raw_text_file = gr.Dropdown(choices=utils.get_datasets('training/datasets', 'txt'), value='None', label='Text file', info='The raw text file to use for training.')
+            create_refresh_button(raw_text_file, lambda: None, lambda: {'choices': utils.get_datasets('training/datasets', 'txt')}, 'refresh-button')
+
+        with gr.Row():
+            micro_batch_size = gr.Slider(label='Micro Batch Size', value=4, minimum=1, maximum=128, step=1, info='Per-device batch size.')
+            batch_size = gr.Slider(label='Batch Size', value=128, minimum=0, maximum=1024, step=4, info='Global batch size.', interactive=False )
+        
+
+        #with gr.Row():
+        #    cutoff_len = gr.Slider(label='Cutoff Length', minimum=0, maximum=2048, value=256, step=32, info='Cutoff length for text input.')
+        #    overlap_len = gr.Slider(label='Overlap Length', minimum=0, maximum=512, value=128, step=16, info='Overlap length')
+
+        with gr.Row():
+            steps = gr.Slider(label='Desired Max Training steps (around 1500-2000 is usually decent)', minimum=500, maximum=8000, value=2000, step=100, info='Max Steps')
+        with gr.Row():
+            estimate = gr.Button("Estimate Epochs")
+            result = gr.Textbox(label='Epochs to hit Max Steps', value='', info='Estimate number of epochs needed to reach the Max Steps')
+       
+        estimate.click(estimate_proc, inputs=[raw_text_file, micro_batch_size,batch_size, steps], outputs=result)        
 
